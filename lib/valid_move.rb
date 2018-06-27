cbec4a2af214b96cde48f08fbf571ad043eec816
Tammy Metz <tammy@tammymetz.com>
2018-06-27 14:18:30 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,22 @@
 # code your #valid_move? method here
-
+def valid_move?(board,index)
+  return_value = false
+  if index.between?(0,8)
+    return_value = true
+  end
+  return return_value && !position_taken?(board, index) # position_taken? returns false if the square is available, so need to negate that
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  return_value = false
+  if board[index] == "X" || board[index] == "O"
+    return_value = true
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
+    return_value = false
+  else
+    return_value = false
+  end
+  return return_value
+end
